6c58e8b6b4f2335aceccc65defd9cbdfcc7f8442
ElderMael <sftwr.mael@gmail.com>
2020-11-14 20:18:45 -0600

    Add Code To Test gpio Sensor

--- index.ts
+++ index.ts
@@ -1,21 +1,44 @@
 import express from "express";
 import PromExporter from "@tailorbrands/node-exporter-prometheus"
-// `appName` is the name of your service/application
+import rpio from "rpio";
 
 const app = express();
 const port = 8080;
 
-const options = {
+const promExporter = PromExporter({
     appName: 'co2-sensor-pi'
-};
+});
 
-console.log(PromExporter);
+const simpleCounter = new promExporter.promClient.Gauge({
+    name: 'simple_counter',
+    help: 'One route increases another one decreases'
+});
 
-const promExporter = PromExporter(options);
-
+app.use((req, res, next) => {
+    try {
+        const rxbuf = new Buffer(32);
+        const bytes = rpio.i2cRead(rxbuf, 16).toString();
+        console.log('Bytes read from gpio sensor:', bytes);
+    } finally {
+        simpleCounter.set();
+        next();
+    }
+});
 app.use(promExporter.middleware);
+
 app.get('/metrics', promExporter.metrics);
 
+app.on('listening', () => {
+    rpio.i2cBegin();
+
+    rpio.i2cSetSlaveAddress(0x20);
+
+    rpio.i2cSetBaudRate(50000);
+});
+
+app.on('close', () => {
+    rpio.i2cEnd();
+});
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
