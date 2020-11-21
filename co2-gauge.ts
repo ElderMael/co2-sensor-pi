b2032d4cf3a9a6ce923a3958f0d66a54666e2103
ElderMael <sftwr.mael@gmail.com>
2020-11-21 08:48:19 -0600

    Fix Class Names For Counter

--- co2-gauge.ts
+++ co2-gauge.ts
@@ -9,31 +9,31 @@ export const co2Gauge = new promExporter.client.Gauge({
     help: 'Carbon Dioxide PPM'
 });
 
-export const messageInvalidErrorCounter = new promExporter.clientCounter({
+export const messageInvalidErrorCounter = new promExporter.client.Counter({
    name: 'message_invalid_errors',
 });
 
-export const readRegisterInvalidErrorCounter = new promExporter.clientCounter({
+export const readRegisterInvalidErrorCounter = new promExporter.client.Counter({
     name: 'read_reg_invalid_errors',
 });
 
-export const measurementModeInvalidErrorCounter = new promExporter.clientCounter({
+export const measurementModeInvalidErrorCounter = new promExporter.client.Counter({
     name: 'meas_mode_invalid_errors',
 });
 
-export const maxResistanceErrorCounter = new promExporter.clientCounter({
+export const maxResistanceErrorCounter = new promExporter.client.Counter({
     name: 'max_resistance_errors',
 });
 
-export const heaterFaultErrorCounter = new promExporter.clientCounter({
+export const heaterFaultErrorCounter = new promExporter.client.Counter({
     name: 'heater_fault_errors',
 });
 
-export const heaterSupplyErrorCounter = new promExporter.clientCounter({
+export const heaterSupplyErrorCounter = new promExporter.client.Counter({
     name: 'heater_supply_errors',
 });
 
-export const unknownErrorCounter = new promExporter.clientCounter({
+export const unknownErrorCounter = new promExporter.client.Counter({
     name: 'unknown_errors',
 });
 
